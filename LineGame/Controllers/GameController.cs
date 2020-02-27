f42acc93b879da1e161a349179b310cac2fb6372
Jonnie <jcamp@mestek.com>
2020-02-27 09:12:18 -0500

    Add project files.

---
+++ LineGame/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using LineGame.Models;
+
+namespace LineGame.Controllers
+{
+    public class GameController : ApiController
+    {
+        public IHttpActionResult Initialize()
+        {
+            //link for information https://docs.microsoft.com/en-us/aspnet/web-api/overview/getting-started-with-aspnet-web-api/tutorial-your-first-web-api
+            //look at the routing link so that the requests can be the right URI's
+            //this also returns nothing right now need to fix
+            return Ok(new Response());
+        }
+    }
+}
---
+++ LineGame/Models/Response.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LineGame.Models
+{
+    public class Response
+    {
+        public string msg { get; set; }
+        //dont stay as an object need to make a different response model for each type of response
+        public object body { get; set; }
+    }
+}
