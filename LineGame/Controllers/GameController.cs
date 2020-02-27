d565021b3d8240adf1b26433edca0c7714111884
Jonnie <jcamp@mestek.com>
2020-02-27 14:40:17 -0500

    Updated nuget packages to get code to run correctly

--- LineGame/Controllers/GameController.cs
+++ LineGame/Controllers/GameController.cs
@@ -8,20 +8,24 @@ using LineGame.Models;
 
 namespace LineGame.Controllers
 {
+    //https://stackoverflow.com/questions/14811772/how-to-use-caching-in-asp-net-web-api
+    //https://stackoverflow.com/questions/5939353/how-do-you-keep-the-value-of-global-variables-between-different-action-methods-c
     public class GameController : ApiController
     {
-        //can either use a private static varibale representing what nodes have been clicked already or can use session caching to store what has already been clicked
+        //can either use a private static variable representing what nodes have been clicked already or can use session caching to store what has already been clicked
+        [HttpGet]
         public IHttpActionResult Initialize()
         {
             //link for information https://docs.microsoft.com/en-us/aspnet/web-api/overview/getting-started-with-aspnet-web-api/tutorial-your-first-web-api
             //look at the routing link so that the requests can be the right URI's
             //this also returns nothing right now need to fix
-            return Ok(new InitializeResponse());
+            return Json(new InitializeResponse());
         }
         //have to find out where the current client map can be referenced so I can reference all the points that were clicked
+        [HttpPost]
         public IHttpActionResult NodeClick()
         {
-            return Ok(new ServerResponse());
+            return Json(new ServerResponse());
         }
     }
 }
